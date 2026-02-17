ab87923c81fc0059c743d11e6566b6b8981d8cbe
Christopher Bonnin <chrisbit.carrington@gmail.com>
2026-02-17 09:02:30 +0000

    updated dbt models

--- models/marts/mart_monthly_retention.sql
+++ models/marts/mart_monthly_retention.sql
@@ -1,6 +1,6 @@
 WITH user_metrics AS (
     SELECT
-        DATE_TRUNC(create_at, MONTH) AS order_month,
+        DATE_TRUNC(created_at, MONTH) AS order_month,
         COUNT(DISTINCT user_id) AS total_monthly_customers,
         SUM(is_repeat_purchase) AS repeat_customers
     FROM {{ref('int_user_order_sequencing')}}
@@ -13,5 +13,4 @@ SELECT
     -- The core KPI: Repeat Purchase Rate
     ROUND(SAFE_DIVIDE(repeat_customers,total_monthly_customers)* 100,2) AS repeat_purchase_rate_pct
 FROM user_metrics
-ORDER BY
-    1 DESC;
+ORDER BY 1 DESC
